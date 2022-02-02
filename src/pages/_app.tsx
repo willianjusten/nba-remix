0dbfe8edd8e4911d12ecf73c07394faa609adce2
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2022-02-02 19:21:56 -0300

    Add react-query and initial structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import 'styles/globals.css'
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const queryClient = new QueryClient()
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp

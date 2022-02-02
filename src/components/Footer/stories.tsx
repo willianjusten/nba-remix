0e474c234a54715747074cea43963a23bc2cec73
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2022-02-02 20:29:08 -0300

    Remote unused files

--- src/components/Footer/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react'
-import Footer from '.'
-
-export default {
-  title: 'Footer',
-  component: Footer
-} as Meta
-
-export const Default: Story = () => <Footer />

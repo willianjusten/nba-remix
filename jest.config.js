12cfa6efea588c842dff1cf3ce7aa0fadb6313c4
Helder Burato Berto <helder.burato@gmail.com>
2022-02-06 21:25:00 +0000

    Add Jest + React Testing Library configuration
    Closes #21

---
+++ jest.config.js
@@ -0,0 +1,38 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  // A list of paths to directories that Jest should use to search for files in
+  roots: ['<rootDir>'],
+
+  // The test environment that will be used for testing
+  testEnvironment: 'jsdom',
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  collectCoverageFrom: [
+    'app/**/*.ts(x)',
+    'api/**/*.(t|j)sx?$',
+    '!app/root.tsx',
+    '!app/entry.client.tsx',
+    '!app/entry.server.tsx',
+  ],
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+  testPathIgnorePatterns: ['/node_modules/', '.cache/'],
+
+  // A map from regular expressions to paths to transformers
+  transform: {
+    '^.+\\.(t|j)sx?$': '@swc/jest',
+  },
+
+  // A list of paths to modules that run some code to configure or set up the testing framework before each test
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+}

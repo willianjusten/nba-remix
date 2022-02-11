24c35226da2d38438e9d33eb1ef81e011fe6ac3d
Willian Justen <willianjustenqui@gmail.com>
2022-02-11 17:18:26 -0300

    Add team logos - closes #32 (#33)
    * Add react-nba-logos package
    * Add TeamLogo component to handle NBA logos
    * Add Storybook team enum

---
+++ app/components/TeamLogo/stories.tsx
@@ -0,0 +1,51 @@
+import { Story, Meta } from '@storybook/react'
+import TeamLogo, { TeamLogoProps } from '.'
+
+export default {
+  title: 'TeamLogo',
+  component: TeamLogo,
+  argTypes: {
+    team: {
+      options: [
+        'ATL',
+        'BKN',
+        'BOS',
+        'CHA',
+        'CHI',
+        'CLE',
+        'DAL',
+        'DEN',
+        'DET',
+        'GSW',
+        'HOU',
+        'IND',
+        'LAC',
+        'LAL',
+        'MEM',
+        'MIA',
+        'MIL',
+        'MIN',
+        'NOP',
+        'NYK',
+        'OKC',
+        'ORL',
+        'PHI',
+        'PHX',
+        'POR',
+        'SAC',
+        'SAS',
+        'TOR',
+        'UTA',
+        'WAS',
+      ],
+      control: { type: 'select' },
+    },
+  },
+} as Meta
+
+export const Default: Story<TeamLogoProps> = (args) => <TeamLogo {...args} />
+
+Default.args = {
+  team: 'GSW',
+  size: 300,
+}

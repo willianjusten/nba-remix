9cdce4968794e338ac6a12f15132c34d296d337b
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2022-02-11 14:57:12 -0300

    Configure Jest timezone
    See more: https://stackoverflow.com/questions/56261381/how-do-i-set-a-timezone-in-my-jest-config

---
+++ .jest/globalSetup.js
@@ -0,0 +1,3 @@
+module.exports = async () => {
+  process.env.TZ = 'UTC-03:00' // Brazil Timezone
+}
--- .jest/setup.ts
+++ .jest/setup.ts
@@ -1,5 +1,1 @@
 import '@testing-library/jest-dom'
-
-module.exports = async () => {
-  process.env.TZ = 'UTC'
-}

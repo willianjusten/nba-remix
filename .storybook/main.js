71316c957d4ee46cb920158c3f82023f4c7f598d
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2022-02-13 15:45:29 -0300

    Update Storybook config to work with remix alias and Tailwind

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,30 @@
+const path = require('path')
+
 module.exports = {
   staticDirs: ['../public'],
   stories: ['../app/components/**/stories.tsx'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-postcss'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        cssLoaderOptions: {
+          // When you have splitted your css over multiple files
+          // and use @import('./other-styles.css')
+          importLoaders: 1,
+        },
+        postcssLoaderOptions: {
+          // When using postCSS 8
+          implementation: require('postcss'),
+        },
+      },
+    },
+  ],
   framework: '@storybook/react',
+  webpackFinal: (config) => {
+    config.resolve.alias['~'] = path.resolve(__dirname, '../app')
+    config.resolve.alias['~/utils'] = path.resolve(__dirname, '../app/utils')
+    return config
+  },
 }
-

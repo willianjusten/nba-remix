b7cf62376bf3dbb9a6d6307864299b127ab9c6c2
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2022-02-14 15:14:26 -0300

    Add TeamLogo where the logo does not exist

--- app/components/TeamLogo/index.tsx
+++ app/components/TeamLogo/index.tsx
@@ -8,7 +8,13 @@ export type TeamLogoProps = {
 function TeamLogo({ team, size }: TeamLogoProps) {
   const Icon = NBAIcons[team as keyof typeof NBAIcons]
 
-  return <Icon size={size} />
+  return Icon ? (
+    <Icon size={size} />
+  ) : (
+    <div className="flex h-16 w-16 items-center justify-center rounded-full bg-orange-400 text-xl font-bold text-gray-900">
+      {team}
+    </div>
+  )
 }
 
 export default TeamLogo

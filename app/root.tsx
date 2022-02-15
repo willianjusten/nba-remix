57192401173bdb0dc83b2f5faaca105c60da6e13
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2022-02-15 16:07:32 -0300

    Add Nprogress to show animation between page transitions - closes #40

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,17 @@
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from 'remix'
+import NProgress from 'nprogress'
+import nProgressStyles from 'nprogress/nprogress.css'
+
+import { useEffect } from 'react'
+
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useTransition,
+} from 'remix'
 import type { MetaFunction } from 'remix'
 
 import styles from './tailwind.css'
@@ -8,10 +21,23 @@ export const meta: MetaFunction = () => {
 }
 
 export function links() {
-  return [{ rel: 'stylesheet', href: styles }]
+  return [
+    { rel: 'stylesheet', href: styles },
+    { rel: 'stylesheet', href: nProgressStyles },
+  ]
 }
 
 export default function App() {
+  let transition = useTransition()
+
+  useEffect(() => {
+    // when the state is idle then we can to complete the progress bar
+    if (transition.state === 'idle') NProgress.done()
+    // and when it's something else it means it's either submitting a form or
+    // waiting for the loaders of the next location so we start it
+    else NProgress.start()
+  }, [transition.state])
+
   return (
     <html lang="en">
       <head>

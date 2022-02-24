131e180e114871f5233afbc1b60b0db72285a4fd
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2022-02-24 09:57:14 -0300

    Add Links to Header
    I needed to remove stories from Header and Layout because Link doesn't work yet

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,7 +1,23 @@
+import { Link } from 'remix'
+
 function Header() {
   return (
-    <header className="container mx-auto px-4 py-8">
-      <img className="w-24" src="/images/nba-logo.svg" alt="NBA Remix" />
+    <header className="container mx-auto flex items-center justify-between px-4 py-8">
+      <Link to="/">
+        <img className="w-24" src="/images/nba-logo.svg" alt="NBA Remix" />
+      </Link>
+
+      <nav className="flex gap-6">
+        <Link className="text-lg transition-opacity hover:opacity-70" to="/">
+          Home
+        </Link>
+        <Link
+          className="text-lg transition-opacity hover:opacity-70"
+          to="/standings"
+        >
+          Standings
+        </Link>
+      </nav>
     </header>
   )
 }
--- app/components/Header/test.tsx
+++ app/components/Header/test.tsx
@@ -1,11 +1,19 @@
 import { render, screen } from '@testing-library/react'
+import { LinkProps } from 'remix'
 
 import Header from '.'
+
+jest.mock('remix', () => ({
+  Link: ({ children }: LinkProps) => <a href="/">{children}</a>,
+  useHref: jest.fn(),
+}))
 
 describe('<Header />', () => {
   it('should render correctly', () => {
     render(<Header />)
 
     expect(screen.getByRole('img', { name: /NBA Remix/i })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /home/i })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /standings/i })).toBeInTheDocument()
   })
 })

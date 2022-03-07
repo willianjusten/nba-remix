cddf7aad05e2eb4763b649d9553b975dc576b8db
Gustavo Guichard <gustavoguichard@gmail.com>
2022-03-07 11:43:16 -0300

    Add prefetch to links for a little speed boost

--- app/components/DateSelector/index.tsx
+++ app/components/DateSelector/index.tsx
@@ -17,7 +17,7 @@ function DateSelector({ day, nextDay, prevDay }: DateSelectorProps) {
     <div className="flex flex-col py-12">
       <h1 className="mb-3 text-4xl font-bold">Games</h1>
       <div className="flex items-center gap-5">
-        <Link to={`/${format(prevDay, DATE_LINK_FORMAT)}`}>
+        <Link prefetch="intent" to={`/${format(prevDay, DATE_LINK_FORMAT)}`}>
           <ArrowIcon title="previous day" />
         </Link>
 
@@ -32,7 +32,7 @@ function DateSelector({ day, nextDay, prevDay }: DateSelectorProps) {
           }
         />
 
-        <Link to={`/${format(nextDay, DATE_LINK_FORMAT)}`}>
+        <Link prefetch="intent" to={`/${format(nextDay, DATE_LINK_FORMAT)}`}>
           <ArrowIcon className="rotate-180" title="next day" />
         </Link>
       </div>

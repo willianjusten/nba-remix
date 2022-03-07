a9defc454083d564203fc6165d0baaaca6060439
Alan Gabriel <contatoalangabriel@gmail.com>
2022-03-07 11:39:50 -0300

    chore: add padding on arrows

--- app/components/DateSelector/index.tsx
+++ app/components/DateSelector/index.tsx
@@ -16,8 +16,8 @@ function DateSelector({ day, nextDay, prevDay }: DateSelectorProps) {
   return (
     <div className="flex flex-col py-12">
       <h1 className="mb-3 text-4xl font-bold">Games</h1>
-      <div className="flex items-center gap-5">
-        <Link to={`/${format(prevDay, DATE_LINK_FORMAT)}`}>
+      <div className="-ml-2 flex items-center gap-5">
+        <Link className="p-2" to={`/${format(prevDay, DATE_LINK_FORMAT)}`}>
           <ArrowIcon title="previous day" />
         </Link>
 
@@ -32,7 +32,7 @@ function DateSelector({ day, nextDay, prevDay }: DateSelectorProps) {
           }
         />
 
-        <Link to={`/${format(nextDay, DATE_LINK_FORMAT)}`}>
+        <Link className="p-2" to={`/${format(nextDay, DATE_LINK_FORMAT)}`}>
           <ArrowIcon className="rotate-180" title="next day" />
         </Link>
       </div>

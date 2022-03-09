fb04d65b4f987ce0fc955afcba298a1d3cf31145
Adeonir Kohl <adeonir@gmail.com>
2022-03-08 22:24:22 -0300

    Add opacity to score of looser team

--- app/components/GameCard/index.tsx
+++ app/components/GameCard/index.tsx
@@ -1,7 +1,7 @@
 import cn from 'classnames'
 
 import TeamInfo from '../TeamInfo'
-import type { Game } from '~/types'
+import type { Game, Team } from '~/types'
 import { getTimePeriod } from '~/utils/handleApiDates'
 
 export type GameCardProps = Game & {
@@ -20,54 +20,75 @@ const GameCard = ({
   clock,
   details = true,
   interactive = true,
-}: GameCardProps) => (
-  <article
-    className={cn(
-      'flex rounded-lg border border-main bg-glass text-white backdrop-blur-lg duration-300 firefox:bg-slate-750',
-      {
-        'hover:cursor-pointer hover:bg-slate-700': interactive,
-      },
-    )}
-  >
-    <div className="flex w-full flex-col">
-      <div className="flex p-6">
-        <TeamInfo team={vTeam} />
+}: GameCardProps) => {
+  function getWinner(vTeam: Team, hTeam: Team) {
+    if (Number(hTeam.score) === Number(vTeam.score)) return null
+    return Number(hTeam.score) > Number(vTeam.score) ? 'hTeam' : 'vTeam'
+  }
 
-        <div className="mt-3 flex flex-1">
-          {vTeam.score && (
-            <p className="w-1/3 text-left text-2xl font-bold">{vTeam.score}</p>
-          )}
-          <p className="flex-1 whitespace-nowrap px-3 pt-1.5 text-center uppercase">
-            {getTimePeriod({
-              startTime,
-              status,
-              period,
-              clock,
-              isHalftime,
-              isEndOfPeriod,
-            })}
-            {status == 2 && (
-              <span className="mx-auto block pt-2 text-xs tracking-widest">
-                <span className="mr-1 inline-block h-2 w-2 animate-pulse rounded-full bg-red-700"></span>
-                Live
-              </span>
+  const winner = getWinner(vTeam, hTeam)
+
+  return (
+    <article
+      className={cn(
+        'flex rounded-lg border border-main bg-glass text-white backdrop-blur-lg duration-300 firefox:bg-slate-750',
+        {
+          'hover:cursor-pointer hover:bg-slate-700': interactive,
+        },
+      )}
+    >
+      <div className="flex w-full flex-col">
+        <div className="flex p-6">
+          <TeamInfo team={vTeam} />
+
+          <div className="mt-3 flex flex-1">
+            {vTeam.score && (
+              <p
+                className={cn('w-1/3 text-left text-2xl font-bold', {
+                  'opacity-50': winner !== 'vTeam' && status === 3,
+                })}
+              >
+                {vTeam.score}
+              </p>
             )}
-          </p>
-          {hTeam.score && (
-            <p className="w-1/3 text-right text-2xl font-bold">{hTeam.score}</p>
-          )}
+            <p className="flex-1 whitespace-nowrap px-3 pt-1.5 text-center uppercase">
+              {getTimePeriod({
+                startTime,
+                status,
+                period,
+                clock,
+                isHalftime,
+                isEndOfPeriod,
+              })}
+              {status == 2 && (
+                <span className="mx-auto block pt-2 text-xs tracking-widest">
+                  <span className="mr-1 inline-block h-2 w-2 animate-pulse rounded-full bg-red-700"></span>
+                  Live
+                </span>
+              )}
+            </p>
+            {hTeam.score && (
+              <p
+                className={cn('w-1/3 text-right text-2xl font-bold', {
+                  'opacity-50': winner !== 'hTeam' && status === 3,
+                })}
+              >
+                {hTeam.score}
+              </p>
+            )}
+          </div>
+
+          <TeamInfo team={hTeam} />
         </div>
 
-        <TeamInfo team={hTeam} />
+        {details && (
+          <footer className="border-t border-main py-2 text-center text-sm">
+            View details
+          </footer>
+        )}
       </div>
-
-      {details && (
-        <footer className="border-t border-main py-2 text-center text-sm">
-          View details
-        </footer>
-      )}
-    </div>
-  </article>
-)
+    </article>
+  )
+}
 
 export default GameCard

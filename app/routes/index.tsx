35e8c3177f0d0a1025bf8e6de78bd0d4a52c65db
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2022-03-11 01:11:23 -0300

    Remove cache from home and GameDetails as it was creating stale data

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -12,7 +12,6 @@ import { DATE_LINK_FORMAT, TIME_TO_REFETCH } from '~/constants'
 
 import useRevalidateOnInterval from '~/hooks/use-revalidate-on-interval'
 
-import { cachedJson } from '~/utils/cachedJson'
 import { getDays } from '~/utils/handleApiDates'
 import { getSocialMetas, getUrl } from '~/utils/seo'
 
@@ -41,7 +40,7 @@ export const loader: LoaderFunction = async ({ request }) => {
     data: { games },
   } = await API.getGamesByDate(date)
 
-  return cachedJson({ games, requestInfo })
+  return { games, requestInfo }
 }
 
 export default function Index() {

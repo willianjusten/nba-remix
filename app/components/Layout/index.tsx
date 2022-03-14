aa88efb34072a35c96ddea459c91adcc0eef10f3
Flavio Silva <contatoflaviodev@gmail.com>
2022-03-14 18:33:34 -0300

    feat: improve background

--- app/components/Layout/index.tsx
+++ app/components/Layout/index.tsx
@@ -7,10 +7,12 @@ export type LayoutProps = {
 
 export function Layout({ children }: LayoutProps) {
   return (
-    <div className="flex min-h-screen flex-col bg-slate-900 bg-main bg-cover bg-center pb-24 text-white">
-      <Header />
-      <main className="container mx-auto flex-grow px-4">{children}</main>
-      <Footer />
+    <div className="flex min-h-screen flex-col bg-slate-900 bg-main  bg-center bg-no-repeat text-white">
+      <div className="bg-gradient-layout min-h-screen backdrop-blur-bg">
+        <Header />
+        <main className="container mx-auto flex-grow px-4">{children}</main>
+        <Footer />
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,11 +15,18 @@ module.exports = {
       backgroundImage: {
         glass:
           'linear-gradient(262.6deg, rgba(48, 49, 58, 0.5) 0%, rgba(48, 49, 58, 0.3) 101.18%, rgba(48, 49, 58, 0.3) 101.18%);',
-        main: "url('/images/bg.jpg')",
+        main: "url('/images/nba-logo-blue.svg')",
+        teste: "linear-gradient(160deg, rgb(12 43 103 / 30%) 55%, rgb(200 15 46 / 30%) 100%);"
       },
       gridTemplateColumns: {
         'auto-fill': 'repeat(auto-fill, minmax(300px, 1fr))',
       },
+      backdropBlur: {
+        bg: '105px',
+      },
+      background: {
+        teste: "linear-gradient(160deg, rgb(12 43 103 / 30%) 55%, rgb(200 15 46 / 30%) 100%);"
+      }
     },
   },
   plugins: [

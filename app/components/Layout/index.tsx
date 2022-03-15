3ffd191563229aad8265858695b0c861b75a2792
Flavio Silva <contatoflaviodev@gmail.com>
2022-03-15 11:17:25 -0300

    fix: change px blur filter

--- app/components/Layout/index.tsx
+++ app/components/Layout/index.tsx
@@ -7,7 +7,7 @@ export type LayoutProps = {
 
 export function Layout({ children }: LayoutProps) {
   return (
-    <div className="bg-gradient-layout flex min-h-screen flex-col bg-slate-900 text-white before:absolute before:h-full before:w-full before:bg-main before:bg-center  before:bg-no-repeat before:blur-3xl before:content-['']">
+    <div className="bg-gradient-layout flex min-h-screen flex-col bg-slate-900 text-white before:absolute before:h-full before:w-full before:bg-main before:bg-center  before:bg-no-repeat before:blur-[105px] before:content-['']">
       <div className="z-[1]">
         <Header />
         <main className="container mx-auto flex-grow px-4">{children}</main>

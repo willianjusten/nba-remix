cb7839e4ec4577cbb0da82705ec47a2b55f6211a
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2022-03-15 23:04:35 -0300

    Fix some css details

--- app/components/Layout/index.tsx
+++ app/components/Layout/index.tsx
@@ -8,7 +8,7 @@ export type LayoutProps = {
 export function Layout({ children }: LayoutProps) {
   return (
     <div className="bg-layout ">
-      <div className="z-[1]">
+      <div className="z-[1] flex flex-grow flex-col">
         <Header />
         <main className="container mx-auto flex-grow px-4">{children}</main>
         <Footer />

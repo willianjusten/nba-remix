4c5c56df6dbb3bf015f0936223d08777f08b5121
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2022-03-26 21:45:05 -0300

    Fix useTitle that it was getting prevTitle in a wrong manner - lets fallback to the title from meta

--- app/hooks/use-title.ts
+++ app/hooks/use-title.ts
@@ -2,11 +2,6 @@ import { useEffect } from 'react'
 
 export function useTitle(title: string) {
   useEffect(() => {
-    const prevTitle = document.title
     document.title = title
-
-    return () => {
-      document.title = prevTitle
-    }
   }, [title])
 }
